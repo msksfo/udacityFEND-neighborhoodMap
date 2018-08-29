ca746d739f44602578e576f3c1d3f968e9156c6d
Tia Esguerra <msksfo@gmail.com>
2018-08-29 07:30:12 -0700

    Add tab index to sidebar list items for keyboard accessibility

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,7 +11,7 @@ class Sidebar extends Component {
         return (
             // filter by type of site
             <div id="sidebar" className="sidebar"> 
-                <select className="sidebarSelect" aria-label="type of site" onChange={this.props.onChange}>
+                <select id="sidebar-select" className="sidebar-select" aria-label="type of site" onChange={this.props.onChange}>
                     <option value="all">All Sites</option>
                     <option value="cultural">Cultural</option>
                     <option value="natural">Natural</option>
@@ -19,14 +19,14 @@ class Sidebar extends Component {
                 </select>
 
                 {/* if the user has filtered the sites, show the filtered sites. otherwise show all of the sites */}
-                <ul className="sidebar-list" onClick={this.props.onClick} >
+                <ul id="sidebar-list" className="sidebar-list" onKeyPress={this.props.onKeyPress} onClick={this.props.onClick} >
                 { filteredSites.length !== 0  
                     ? filteredSites.map(site => {   
-                        return <li  className="sidebar-listItem" key={site.name}>
+                        return <li  tabIndex="0" className="sidebar-listItem" key={site.name}>
                             {site.name}
                         </li> })    
                     : allSites.map(site => {
-                        return <li className="sidebar-listItem" key={site.name}>
+                        return <li tabIndex="0" className="sidebar-listItem" key={site.name}>
                             {site.name}
                         </li>})
                 }  
