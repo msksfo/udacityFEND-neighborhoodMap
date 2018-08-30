6d52cf88be16b433ad5e80a1f4bf40a5d04e4b99
Tia Esguerra <msksfo@gmail.com>
2018-08-30 10:52:31 -0700

    refactor: Make Header component a functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../App.css';
 
-class Header extends Component {
-    render() {
-        return (
-            <header className="header">
-                   
-                <div className="title-div">
-                    <h1 className="title">Our Global Neighborhood</h1>
-                    <h2 className='subheading'>Explore the UNESCO World Heritage Sites</h2>
-                </div> 
-                   
-            </header>
-        ) 
-    }
+function Header() {
+    
+    return (
+        <header className="header">
+                
+            <div className="title-div">
+                <h1 className="title">Our Global Neighborhood</h1>
+                <h2 className='subheading'>Explore the UNESCO World Heritage Sites</h2>
+            </div> 
+                
+        </header>
+    )   
 }
 
 export default Header;
