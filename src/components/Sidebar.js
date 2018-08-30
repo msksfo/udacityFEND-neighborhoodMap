28a21f7f0f2dc3705e16d18ae87e32b693673ffe
Tia Esguerra <msksfo@gmail.com>
2018-08-30 10:49:39 -0700

    refactor: Make Sidebar component a functional component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,50 +1,47 @@
-import React, { Component } from 'react'
+import React from 'react'
 import '../App.css'
 
-class Sidebar extends Component {
+function Sidebar (props) {
     
-    render(){
-        const allSites = this.props.allSites;
-        const filteredSites = this.props.filteredSites;
+    const allSites = props.allSites;
+    const filteredSites = props.filteredSites;
 
+    return (
+        // filter by type of site
+        <div id="sidebar" className="sidebar"> 
+            <select id="sidebar-select" className="sidebar-select" aria-label="type of site" onChange={props.onChange}>
+                <option value="all">All Sites</option>
+                <option value="cultural">Cultural Sites</option>
+                <option value="natural">Natural Sites</option>
+                <option value="mixed">Mixed Sites</option>
+            </select>
 
-        return (
-            // filter by type of site
-            <div id="sidebar" className="sidebar"> 
-                <select id="sidebar-select" className="sidebar-select" aria-label="type of site" onChange={this.props.onChange}>
-                    <option value="all">All Sites</option>
-                    <option value="cultural">Cultural Sites</option>
-                    <option value="natural">Natural Sites</option>
-                    <option value="mixed">Mixed Sites</option>
-                </select>
-
-                {/* if the user has filtered the sites, show the filtered sites. otherwise show all of the sites */}
-                <ul id="sidebar-list" className="sidebar-list" onKeyPress={this.props.onKeyPress} onClick={this.props.onClick} >
-                    {
-                        filteredSites.length !== 0 
-                        ? filteredSites.map( site => {
-                            if (site.type === 'natural'){
-                                return <li tabIndex="0" key={site.name} className={'sidebar-listItem natural'}>{site.name}</li>                         
-                            } else if (site.type === 'cultural'){
-                                return <li tabIndex="0" key={site.name} className={'sidebar-listItem cultural'}>{site.name}</li>
-                            } else {
-                                return <li tabIndex="0" key={site.name} className={'sidebar-listItem mixed'}>{site.name}</li>
-                            }
-                        })
-                        : allSites.map( site => {
-                            if (site.type === 'natural'){
-                                return <li tabIndex="0" key={site.name} className={'sidebar-listItem natural'}>{site.name}</li>                         
-                            } else if (site.type === 'cultural'){
-                                return <li tabIndex="0" key={site.name} className={'sidebar-listItem cultural'}>{site.name}</li>
-                            } else {
-                                return <li tabIndex="0" key={site.name} className={'sidebar-listItem mixed'}>{site.name}</li>
-                            }
-                        })
-                    }
-                </ul>
-            </div>
-        )
-    }
+            {/* if the user has filtered the sites, show the filtered sites. otherwise show all of the sites */}
+            <ul id="sidebar-list" className="sidebar-list" onKeyPress={props.onKeyPress} onClick={props.onClick} >
+                {
+                    filteredSites.length !== 0 
+                    ? filteredSites.map( site => {
+                        if (site.type === 'natural'){
+                            return <li tabIndex="0" key={site.name} className={'sidebar-listItem natural'}>{site.name}</li>                         
+                        } else if (site.type === 'cultural'){
+                            return <li tabIndex="0" key={site.name} className={'sidebar-listItem cultural'}>{site.name}</li>
+                        } else {
+                            return <li tabIndex="0" key={site.name} className={'sidebar-listItem mixed'}>{site.name}</li>
+                        }
+                    })
+                    : allSites.map( site => {
+                        if (site.type === 'natural'){
+                            return <li tabIndex="0" key={site.name} className={'sidebar-listItem natural'}>{site.name}</li>                         
+                        } else if (site.type === 'cultural'){
+                            return <li tabIndex="0" key={site.name} className={'sidebar-listItem cultural'}>{site.name}</li>
+                        } else {
+                            return <li tabIndex="0" key={site.name} className={'sidebar-listItem mixed'}>{site.name}</li>
+                        }
+                    })
+                }
+            </ul>
+        </div>
+    )   
 }
 
 export default Sidebar;
