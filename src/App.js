e96b938723f4c3e96133324109dd57cbfbe074df
Tia Esguerra <msksfo@gmail.com>
2018-08-30 10:44:26 -0700

    refactor: Make the App component a functional commponent

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-   
-  }
-
-  
-  render() {
-    return (
-      <div className="app">
-      
+function App () {
+ 
+  return (
+    <div className="app"> {/* am i using this className??? */}
+    
         <Header />
 
         <Main />
 
         <Footer />
 
-      </div>
-    );
-  }
+    </div>
+  );
+  
 }
 
 export default App
