4c3dd9d6c1e9e1a5dc62f19fdddd81b82d8293fe
Tia Esguerra <msksfo@gmail.com>
2018-08-30 10:52:05 -0700

    refactor: Make Footer component a functional component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../App.css';
 
-class Footer extends Component {
-    render(){
-        return (
-            <div className="footer">
-                <p className="quote"><q>From country to country, languages differ but the sight of the Moon is the same and the hearts of men are one!</q> ~ Ki no Tsurayuki</p>
-                <p className="learn-more">Learn more about the UNESCO World Heritage Sites <a target='_blank' href='https://whc.unesco.org/en/list/' rel='noopener noreferrer'>here</a> (link opens a new window)</p>
-                <p className="api-attribution">This product uses the Flickr API but is not endorsed or certified by SumgMug, Inc.</p>
-            </div>
-        )
-    }
+function Footer() {
+   
+    return (
+        <div className="footer">
+            <p className="quote"><q>From country to country, languages differ but the sight of the Moon is the same and the hearts of men are one!</q> ~ Ki no Tsurayuki</p>
+            <p className="learn-more">Learn more about the UNESCO World Heritage Sites <a target='_blank' href='https://whc.unesco.org/en/list/' rel='noopener noreferrer'>here</a> (link opens a new window)</p>
+            <p className="api-attribution">This product uses the Flickr API but is not endorsed or certified by SumgMug, Inc.</p>
+        </div>
+    )
 }
 
 export default Footer
