c22921a2a18914d36c866251cb4f6d7308aa830e
Tia Esguerra <msksfo@gmail.com>
2018-08-31 09:36:31 -0700

    style: Remove subheading from Header. Add background images

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,27 @@ function Header() {
     
     return (
         <header className="header">
+            <div className="photo1"></div>
+            <div className="photo2"></div>
+            <div className="photo3"></div>
+            <div className="photo4"></div>
+            <div className="photo5"></div>
+            <div className="photo6"></div>
+            <div className="photo7"></div>
+            <div className="photo8"></div>
+            <div className="photo9"></div>
+            <div className="photo10"></div>
+            <div className="photo11"></div>
+            <div className="photo12"></div>
+            <div className="photo13"></div>
+            <div className="photo14"></div>
+            <div className="photo15"></div>
+            <div className="photo16"></div>
+            <div className="photo17"></div>
+            <div className="photo18"></div>
                 
             <div className="title-div">
                 <h1 className="title">Our Global Neighborhood</h1>
-                <h2 className='subheading'>Explore the UNESCO World Heritage Sites</h2>
             </div> 
                 
         </header>
